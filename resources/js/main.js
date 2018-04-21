346507754ddc441805169086e4957c290cfcb7c3
Bezlonir <michael_bragg99@hotmail.com>
2018-04-21 17:16:09 -0600

    Finish slick jumbotron with responsive styling.

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.bragg-group-carousel').slick({
+    accessibility:false,
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed:6000,
+    draggable:false,
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    swipe:false,
+    touchMove:false
+  });
+});
