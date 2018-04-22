3e7e26d9368f9515c376fafd69856df661b165a0
Bezlonir <michael_bragg99@hotmail.com>
2018-04-22 08:12:14 -0600

    Implement styling and javascript through video section.

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,4 +10,44 @@ $(document).ready(function(){
     swipe:false,
     touchMove:false
   });
+  sizeIntroVid();
+  sizeAboutVid();
+  $(window).resize(function(){
+    sizeIntroVid();
+    sizeAboutVid();
+  });
 });
+
+// set the size of the YouTube video
+function sizeIntroVid() {
+  $('.intro-video').val('');
+  var introWidth = $('.intro-column').width();
+  introWidth = Math.floor(introWidth* .95);
+  var introHeight = Math.floor(introWidth * 0.6015625);
+  var introHTML =
+  '<iframe ' +
+      'width="'+ introWidth +'"' +
+      'height="'+ introHeight +'"' +
+      'src="https://www.youtube.com/embed/MEX_SrCI4VA"' +
+      'frameborder="0"' +
+      'allow="autoplay; encrypted-media"' +
+      'allowfullscreen></iframe>';
+  $('.intro-video').html(introHTML);
+};
+
+// set the size of the YouTube video
+function sizeAboutVid() {
+  $('.about-video').val('');
+  var aboutWidth = $('.about-column').width();
+  aboutWidth = Math.floor(aboutWidth * .95);
+  var aboutHeight = Math.floor(aboutWidth * 0.6015625);
+  var aboutHTML =
+  '<iframe ' +
+      'width="'+ aboutWidth +'"' +
+      'height="'+ aboutHeight +'"' +
+      'src="https://www.youtube.com/embed/65F0kikq-iY"' +
+      'frameborder="0"' +
+      'allow="autoplay; encrypted-media"' +
+      'allowfullscreen></iframe>';
+  $('.about-video').html(aboutHTML);
+};
